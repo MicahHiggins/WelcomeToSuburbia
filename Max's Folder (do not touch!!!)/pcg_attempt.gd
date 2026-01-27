9b12dbd2780d3d8ca0618d0bc304486e530159f7
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2026-01-26 17:19:27 -0700

    aidanDesign+LoopV2

--- Max's Folder (do not touch!!!)/pcg_attempt.gd
+++ Max's Folder (do not touch!!!)/pcg_attempt.gd
@@ -9,6 +9,8 @@ extends Node3D
 var prevWB
 var worldBlocks2: Array[Node3D]
 
+const DISPLACEMENT = 32
+
 ## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	worldBlocks2 = [world_block_1, world_block_2, world_block_3, 
@@ -26,7 +28,7 @@ func algoForIterations(worldBlocks):
 	var currentWorldBlockIteration = worldBlocks2[worldBlocks].global_position
 	var currentWorldBlockIterationZ = worldBlocks2[worldBlocks].global_position.z
 	var currentWorldBlockIterationX = worldBlocks2[worldBlocks].global_position.x
-	var displacement = 32
+	var displacement = DISPLACEMENT
 	
 	
 	var counter = 0
@@ -35,23 +37,23 @@ func algoForIterations(worldBlocks):
 		if i != worldBlocks:
 			
 			if counter < 2:
-				print("EST")
+				#print("EST")
 				print(counter)
 				print(worldBlocks2[i].position.x)
 				currentWorldBlockIterationX = worldBlocks2[worldBlocks].global_position.x
 				worldBlocks2[i].global_position = currentWorldBlockIteration
 				worldBlocks2[i].global_position.x = currentWorldBlockIterationX + displacement
-				displacement = -32
+				displacement = -DISPLACEMENT
 				counter = counter + 1
 				print(displacement)
 				
 			else:
-				print("F")
+				#print("F")
 				print(worldBlocks2[i].position.z)
 				worldBlocks2[i].global_position = currentWorldBlockIteration
 				currentWorldBlockIterationZ = worldBlocks2[worldBlocks].global_position.z
 				worldBlocks2[i].global_position.z = currentWorldBlockIterationZ + displacement
-				displacement = 32
+				displacement = DISPLACEMENT
 				counter = counter + 1
 				print(displacement)
 				
@@ -62,19 +64,24 @@ func algoForIterations(worldBlocks):
 
 func _on_load_zone_1_body_entered(body: Node3D) -> void:
 	algoForIterations(0)
+	print("numba 0")
 
 
 func _on_load_zone_2_body_entered(body: Node3D) -> void:
 	algoForIterations(1)
+	print("numba 1")
 
 
 func _on_load_zone_3_body_entered(body: Node3D) -> void:
 	algoForIterations(2)
+	print("numba 2")
 
 
 func _on_load_zone_4_body_entered(body: Node3D) -> void:
 	algoForIterations(3)
+	print("numba 3")
 
 
 func _on_load_zone_5_body_entered(body: Node3D) -> void:
 	algoForIterations(4)
+	print("numba 4")
