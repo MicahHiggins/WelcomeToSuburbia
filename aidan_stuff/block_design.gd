6188ac53efcd343c5cac3818f19ba32330b6638b
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2026-02-09 15:54:51 -0700

    PCG collision rescaling

--- aidan_stuff/block_design.gd
+++ aidan_stuff/block_design.gd
@@ -9,6 +9,10 @@ const PATROL_PATH = preload("res://aidan_stuff/patrol_path.tscn")
 
 #var extra_bob = 0
 var patrol_path
+var bob_count
+
+var entered = false
+var exited = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	houses.visible = false
@@ -19,24 +23,30 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	bob_count = get_tree().get_nodes_in_group("your_group_name").size()
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	patrol_path = PATROL_PATH.instantiate()
-	roads.visible = true
-	houses.visible = true
-	add_child(patrol_path)
-	patrol_path.position = Vector3(0, 0, 0)
-	print("2 BOBS?")
+	if entered == false:
+		patrol_path = PATROL_PATH.instantiate()
+		roads.visible = true
+		houses.visible = true
+		add_child(patrol_path)
+		patrol_path.position = Vector3(0, 0, 0)
+		print("2 BOBS?")
+		entered = true
+		exited = false
 
 
 	
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	roads.visible = false
-	houses.visible = false
-	patrol_path.queue_free()
-	print("NO BOBS?")
+	if exited == false:
+		roads.visible = false
+		houses.visible = false
+		patrol_path.queue_free()
+		print("NO BOBS?")
+		exited = true
+		entered = false
 	
