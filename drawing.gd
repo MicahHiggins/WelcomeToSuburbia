ec15af5e7ad4bb2bc234a85b1e0bbb8fa697ee39
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2026-03-04 16:36:43 -0700

    PuzzleV1

---
+++ drawing.gd
@@ -0,0 +1,29 @@
+extends TileMapLayer
+
+
+var gridSize = 20
+var Dict = {}
+
+func _ready() -> void:
+	for x in gridSize:
+		for y in gridSize:
+			var pos = Vector2i(x+20, y)
+			Dict[pos] = {
+				"Type": "Blank"
+			}
+				
+			set_cell(Vector2i(x+20, y), 0, Vector2i(0, 0), 0)
+			
+			
+
+		print(Dict)
+
+
+
+func _process(delta: float) -> void:
+	var tile = local_to_map(get_local_mouse_position())
+	
+	#print(tile)
+	if Dict.has(tile) && Input.is_action_pressed("use-attack"):
+		#print(tile)
+		set_cell(tile, 1, Vector2i(0, 0), 0)
--- symbol_puzzle.gd
+++ symbol_puzzle.gd
@@ -1,16 +1,16 @@
-extends Node3D
-
-@onready var test: Node2D = $CanvasLayer/Control/test
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_obs_area_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
+#extends Node3D
+#
+#@onready var test: Node2D = $CanvasLayer/Control/test
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+#
+#
+#func _on_obs_area_body_entered(body: Node3D) -> void:
+	#pass # Replace with function body.
