5b049ff00c2d80b7a9cc7e931c43098415b2598c
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2026-03-09 12:15:08 -0600

    yipeee!

--- symbolManager.gd
+++ symbolManager.gd
@@ -8,51 +8,98 @@ extends Node3D
 #@onready var puzzle_draw: Node2D = $Drawing
 #@onready var puzzle_see: Node2D = $Seeing
 
+@onready var interact: Label = $interact
+
 @onready var buttons: Control = $Buttons
+
+var in_area = false
+var see = false
+var do = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#GlobalVariables.puzzleType = randi_range(1, 3)
 	print("NUMM:, ", GlobalVariables.puzzleType)
+	seeing.visible = false
+				#exit.visible = false
+				#puzzle_see.visible = false
+	buttons.visible = false
+	drawing.visible = false
+				#exit.visible = false
+				#puzzle_see.visible = false
+	buttons.visible = false
 
 func randPuzzle():
 	GlobalVariables.puzzleType = randi_range(1, 3)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+func _process(delta: float) -> void:
+	print(in_area)
+	if in_area == true:
+		print("HU")
+		interact.visible = true
+	else: 
+		interact.visible = false
+		
 
-
-func _on_obs_area_body_entered(body: Node3D) -> void:
-	print("TESTETES")
-	if body.is_multiplayer_authority():
-		print("IN")
+func _input(event: InputEvent) -> void:
+	if in_area == true && event.is_action("interact") && see == true:
 		seeing.visible = true
 		buttons.visible = true
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif in_area == true && event.is_action_pressed("interact") && do == true:
+		drawing.visible = true
+		buttons.visible = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		#else:
+				#seeing.visible = false
+				##exit.visible = false
+				##puzzle_see.visible = false
+				#buttons.visible = false
+				#drawing.visible = false
+				##exit.visible = false
+				##puzzle_see.visible = false
+				#buttons.visible = false
+		
+		
+		
+		
+func _on_obs_area_body_entered(body: Node3D) -> void:
+	print("TESTETES")
+	if body.is_multiplayer_authority():
+		in_area = true
+		print("IN")
+		see = true
 		#exit.visible = true
 		#puzzle_see.visible = true
 
 func _on_obs_area_body_exited(body: Node3D) -> void:
-		if body.is_multiplayer_authority():
-			seeing.visible = false
-			#exit.visible = false
-			#puzzle_see.visible = false
-			buttons.visible = false
-			#
+	if body.is_multiplayer_authority():
+		in_area = false
+		see = false
+		
 
 
 func _on_do_area_body_entered(body: Node3D) -> void:
-		if body.is_multiplayer_authority():
-			drawing.visible = true
-			buttons.visible = true
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if body.is_multiplayer_authority():
+		in_area = true
+		do = true
+
 		#exit.visible = true
 
 
 func _on_do_area_body_exited(body: Node3D) -> void:
 	if body.is_multiplayer_authority():
-			drawing.visible = false
-			#exit.visible = false
-			#puzzle_see.visible = false
-			buttons.visible = false
+		in_area = false
+		do = false
+
+
+func _on_exit_pressed() -> void:
+	seeing.visible = false
+				#exit.visible = false
+				#puzzle_see.visible = false
+	buttons.visible = false
+	drawing.visible = false
+				#exit.visible = false
+	#puzzle_see.visible = false
+	buttons.visible = false
