3dae1ea76571d02d9956d77b871ee069be192ee4
MicahHiggins <111819983+MicahHiggins@users.noreply.github.com>
2026-03-20 02:37:52 -0600

    oop

--- Max's Folder (do not touch!!!)/pcg_attempt.gd
+++ Max's Folder (do not touch!!!)/pcg_attempt.gd
@@ -203,21 +203,21 @@ func _rpc_request_iteration(zone_idx: int) -> void:
 				iteration5 = 0
 
 
-#func _on_load_zone_1_body_exited(body: Node3D) -> void:
-	#iteration1 = 0
-#
-#
-#func _on_load_zone_2_body_exited(body: Node3D) -> void:
-	#iteration2 = 0
-#
-#
-#func _on_load_zone_3_body_exited(body: Node3D) -> void:
-	#iteration3 = 0
-#
-#
-#func _on_load_zone_4_body_exited(body: Node3D) -> void:
-	#iteration4 = 0
-#
-#
-#func _on_load_zone_5_body_exited(body: Node3D) -> void:
-	#iteration5 = 0
+func _on_load_zone_1_body_exited(body: Node3D) -> void:
+	iteration1 = 0
+
+
+func _on_load_zone_2_body_exited(body: Node3D) -> void:
+	iteration2 = 0
+
+
+func _on_load_zone_3_body_exited(body: Node3D) -> void:
+	iteration3 = 0
+
+
+func _on_load_zone_4_body_exited(body: Node3D) -> void:
+	iteration4 = 0
+
+
+func _on_load_zone_5_body_exited(body: Node3D) -> void:
+	iteration5 = 0
