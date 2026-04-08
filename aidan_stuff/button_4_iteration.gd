a9026435d34749b22e15aa6ff9a1bb0c86a3015c
nowa5285 <nowa5285@colorado.edu>
2026-04-08 16:16:55 -0600

    hmmm

--- aidan_stuff/button_4_iteration.gd
+++ aidan_stuff/button_4_iteration.gd
@@ -1,9 +1,15 @@
 extends Control
 
-
-
+const SERVER_ID := 1
 
 func _on_button_pressed() -> void:
+	if multiplayer.has_multiplayer_peer() and not multiplayer.is_server():
+		rpc_id(SERVER_ID, "_rpc_iter_up")
+		return
+	_rpc_iter_up()
+
+@rpc("any_peer", "call_local", "reliable")
+func _rpc_iter_up() -> void:
 	GlobalVariables.iterations += 1
 	GlobalVariables.ITERS += 1
 	GlobalVariables.gameStart.emit()
