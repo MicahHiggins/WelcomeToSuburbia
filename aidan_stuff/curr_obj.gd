349ffedcfc8d9b0fc635ddbccfd33aca66b3799d
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2026-04-13 11:48:46 -0600

    uiOverlapping BugFix

--- aidan_stuff/curr_obj.gd
+++ aidan_stuff/curr_obj.gd
@@ -11,6 +11,8 @@ var curr_text_main := ""
 var prev_text_side := ""
 var curr_text_side := ""
 var objToggle := false
+
+var isaacQuestDB := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	questHub.iteration_changed.connect(iterationChange)
@@ -23,7 +25,7 @@ func _process(delta: float) -> void:
 	
 	#FIDO FOUND!
 	if objToggle == true:
-		side.text =  "Tell the Campbell Family you found Fido!"
+		side.text =  "Tell the Campbell Family\n you found Fido!"
 	
 
 
@@ -37,26 +39,28 @@ func talkedTo(value: int):
 
 #Iteration change (ref questHub.gd)
 func iterationChange(value: int):
-	if value == 1:
-		prev_text_main = main.text
+	if value == 1 && isaacQuestDB == false:
+		isaacQuestDB = true
+		print("UI Debugging...")
+		prev_text_main = "Find Your Home (130)"
 		main.text = main.text + "\n Talk to Issac (corner)"
 		
 	if value == 2:
 		#main.text = main.text + "\n Talk to Issac (corner)"
-		side.text = "Investigate the Crying (Campbells)"
+		side.text = "Investigate the Crying\n (Campbells)"
 
 func isaacTrigger(value: int):
-	print("Is issac UI ON?")
+	#print("Is issac UI ON?")
 	print("FROM UI: iss quest", GlobalVariables.isaac_quest_progression)
 	if value == 2 && GlobalVariables.isaac_quest_progression == 1:
 		print("ISAAC HAS ENTERED THE UI!")
 		
-		main.text = prev_text_main + "\n explore the neighborhood"
+		main.text = prev_text_main + "\n explore the\n neighborhood"
 		#GlobalVariables.isaac_quest_progression = 2
 		
 	if GlobalVariables.isaac_quest_progression == 3:
 		main.text = prev_text_main + "\n investigate around 111..."
 		
 	elif GlobalVariables.isaac_quest_progression == 5:
-		main.text = prev_text_main + "\n Tell Isaac What You Found"
+		main.text = prev_text_main + "\n Tell Isaac What You\n Found"
 		
