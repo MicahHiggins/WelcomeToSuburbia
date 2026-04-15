2fe4515292d87fd3ccbc83d0a8680dd2544cd1e8
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2026-04-15 15:36:58 -0600

    iterationDetectionSync

--- aidan_stuff/iteration_detector.gd
+++ aidan_stuff/iteration_detector.gd
@@ -1,47 +1,27 @@
 extends Area3D
-var toggle_detection
-@onready var timer: Timer = $Timer
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	timer.wait_time = 30
-	toggle_detection = true
-
-#Little mech for 1st iteration! Placeholder/debugging
-	
-func my_timed_function():
-	print("IterationDQ1")
-	#GlobalVariables.iterations += 1
-	#GlobalVariables.gameStart.emit()
-	questHub.it_change()
-	queue_free()
-	#Might use for differnt ints!
-	#timer.wait_time = 1000
-	#timer.start()
-
-
-
-
-
-
-#
-#func _on_timer_timeout() -> void:
-	#toggle_detection = true
-	
-	
-
+var toggle_detection = true
 
 func _on_body_entered(body: Node3D) -> void:
-	#print("WHAT THE FUCK")
-	if body.is_in_group("player"):
-		
-		if toggle_detection == true:
-			toggle_detection = false
-			print("Before Iter1: ", GlobalVariables.iterations)
-			
-			my_timed_function()
-			
-			print("After Iter2 ", GlobalVariables.iterations)
-		
-		
+	# 1. Only the local player who touched it sends the request
+	if body.is_in_group("player") and body.is_multiplayer_authority():
+		if toggle_detection:
+			toggle_detection = false # Prevent local spam
+			rpc_id(1, "server_trigger_event")
+
+@rpc("any_peer", "call_local", "reliable")
+func server_trigger_event():
+	# 2. Only the server actually processes the logic
+	if not multiplayer.is_server():
+		return
+	
+	print("Server: Triggering global iteration change")
+	questHub.it_change()
+	
+	# 3. Tell everyone to remove this node from their game
+	sync_despawn.rpc()
+
+@rpc("authority", "call_local", "reliable")
+func sync_despawn():
+	print("Despawning trigger for everyone")
+	queue_free()
