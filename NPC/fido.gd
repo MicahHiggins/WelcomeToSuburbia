96d9bce1d5f85624944e09a0c95ecb9f128a0cff
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2026-04-14 20:47:44 -0600

    anothaPush

--- NPC/fido.gd
+++ NPC/fido.gd
@@ -15,7 +15,7 @@ static var fido_toggle := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	found_fido_early = false
+	#found_fido_early = false
 	#collision_shape_3d.set_deferred("disabled", true)
 	questHub.iteration_changed.connect(iterationChange)
 	questHub.campbellQuest.connect(campbellProgression)
