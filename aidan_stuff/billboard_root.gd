8db85e3f50d4d63632d40e24d3f750ef9b81591f
MicahHiggins <mihi2637@colorado.edu>
2026-04-22 16:14:58 -0600

    piec eof shit

--- aidan_stuff/billboard_root.gd
+++ aidan_stuff/billboard_root.gd
@@ -1,25 +1,25 @@
 extends Node3D
 
 @onready var rotator: Node3D = $Rotator
-@onready var label_3d: Label3D = $Rotator/Label3D
-@onready var label_3d2: Label3D = $Rotator/Label3D2
+@onready var sprite_3d: Sprite3D = $Rotator/Sprite3D
+@onready var sprite_3d2: Sprite3D = $Rotator/Sprite3D2
 
-@export var default_text: String = "Welcome to Suburbia!"
+# Default image shown when iteration is not special
+@export var default_texture: Texture2D = preload("res://Assets/Images/Arrow.png")
 
-var unique_iterations := {
-	2: "Where are you going?",
-	4: "He watches from the sun",
-	6: "118"
+# Images for special iterations
+var unique_iteration_images := {
+	2: preload("res://Assets/Images/BB_are_you_lost.webp"),
+	4: preload("res://Assets/Images/BB_where_are_you_going.webp"),
+	6: preload("res://Assets/Images/BB_youll_never_get_home.webp")
 }
 
 var player: Node3D = null
 var last_iteration: int = -1
 
-
 func _ready() -> void:
 	find_player()
-	update_text()
-
+	update_image()
 
 func _process(_delta: float) -> void:
 	if player == null or not is_instance_valid(player):
@@ -28,30 +28,27 @@ func _process(_delta: float) -> void:
 	var iteration: int = GlobalVariables.iterations
 
 	if iteration != last_iteration:
-		update_text()
+		update_image()
 		last_iteration = iteration
 
-	if iteration in unique_iterations and player != null:
+	if iteration in unique_iteration_images and player != null:
 		face_player_y_only()
-
 
 func find_player() -> void:
 	player = get_tree().get_first_node_in_group("player") as Node3D
 
-
-func update_text() -> void:
-	if label_3d == null:
+func update_image() -> void:
+	if sprite_3d == null or sprite_3d2 == null:
 		return
 
 	var iteration: int = GlobalVariables.iterations
+	var texture_to_use: Texture2D = default_texture
 
-	if iteration in unique_iterations:
-		label_3d.text = unique_iterations[iteration]
-		label_3d2.text = unique_iterations[iteration]
-	else:
-		label_3d.text = default_text
-		label_3d2.text = default_text
+	if iteration in unique_iteration_images:
+		texture_to_use = unique_iteration_images[iteration]
 
+	sprite_3d.texture = texture_to_use
+	sprite_3d2.texture = texture_to_use
 
 func face_player_y_only() -> void:
 	var to_player: Vector3 = player.global_position - rotator.global_position
