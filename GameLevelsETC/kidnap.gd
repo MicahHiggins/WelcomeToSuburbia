29dc5ef332960ff7a16f1df8a9f5654c81e516d8
nowa5285 <nowa5285@colorado.edu>
2026-04-22 11:31:24 -0600

    plz i need this

--- GameLevelsETC/kidnap.gd
+++ GameLevelsETC/kidnap.gd
@@ -2,73 +2,70 @@ extends Node3D
 
 @onready var cohesion: AnimationPlayer = $Cohesion
 
+@export var replicate_last_anim_to_late_joiners: bool = true
+
 var _anim_player: AnimationPlayer = null
 var _last_anim: StringName = &""
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if multiplayer.has_multiplayer_peer() and multiplayer.is_server() and replicate_last_anim_to_late_joiners:
+		if not multiplayer.peer_connected.is_connected(_on_peer_connected):
+			multiplayer.peer_connected.connect(_on_peer_connected)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-
 func _on_toy_piano_puzzle_one_complete() -> void:
-	#cohesion.play("puzzle1Complete")
-	_play_anim_server(&"puzzle1Complete")
+	play_anim_networked(&"puzzle1Complete")
 
 func _on_drawing_puzzle_two_complete() -> void:
-	_play_anim_server(&"puzzle2Complete")
-
-#
-#func _play_anim_local(anim_name: StringName) -> void:
-	#if _anim_player == null:
-		#_cache_anim_player()
-
-
-
+	play_anim_networked(&"puzzle2Complete")
 
 func _get_anim_player() -> AnimationPlayer:
 	if _anim_player != null and is_instance_valid(_anim_player):
 		return _anim_player
-
-	# simplest: if the AnimationPlayer is a direct child, use this:
 	_anim_player = get_node_or_null("Cohesion") as AnimationPlayer
 	return _anim_player
 
+func play_anim_networked(anim: StringName) -> void:
+	if multiplayer.has_multiplayer_peer():
+		if multiplayer.is_server():
+			_play_anim_server(anim)
+		else:
+			rpc_id(1, "_rpc_request_play_anim", String(anim))
+	else:
+		_play_anim_local(anim)
 
+@rpc("any_peer", "reliable")
+func _rpc_request_play_anim(anim_name: String) -> void:
+	if not multiplayer.is_server():
+		return
+	_play_anim_server(StringName(anim_name))
 
 func _play_anim_server(anim: StringName) -> void:
-	# 1) if multiplayer, only server is allowed to decide
-	if multiplayer.has_multiplayer_peer() and not multiplayer.is_server():
-		return
-
-	# 2) don't spam the same anim
 	if _last_anim == anim:
 		return
 	_last_anim = anim
 
-	# 3) play locally (server also needs to see it)
-	var ap := _get_anim_player()
-	if ap != null:
-		ap.play(String(anim))
+	_play_anim_local(anim)
 
-	# 4) tell everyone else
 	if multiplayer.has_multiplayer_peer():
 		rpc("_rpc_play_anim", String(anim))
 
-@rpc("any_peer", "call_local", "unreliable")
+@rpc("any_peer", "call_local", "reliable")
 func _rpc_play_anim(anim_name: String) -> void:
-	# server ignores its own packet (optional, but clean)
-	if multiplayer.has_multiplayer_peer() and multiplayer.is_server():
-		return
-
 	_last_anim = StringName(anim_name)
+	_play_anim_local(_last_anim)
 
+func _play_anim_local(anim: StringName) -> void:
 	var ap := _get_anim_player()
-	if ap != null:
-		ap.play(anim_name)
-		
-	
+	if ap == null:
+		return
+	var a := String(anim)
+	if ap.has_animation(a):
+		ap.play(a)
+
+func _on_peer_connected(peer_id: int) -> void:
+	if _last_anim == &"":
+		return
+	rpc_id(peer_id, "_rpc_play_anim", String(_last_anim))
